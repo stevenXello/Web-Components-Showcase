1566707192fbd40173bc4e30e9e7017346f59857
Steven <87040948+stevenXello@users.noreply.github.com>
2021-08-11 13:20:43 -0400

    event listener for button component

--- src/xelloButton.js
+++ src/xelloButton.js
@@ -4,17 +4,34 @@ template.innerHTML = `
     <style>
         @import url('css/xelloButton.css')
     </style>
-   <button></button>
+   <button id="btnClick"></button>
 `;
 
 class XelloButton extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.querySelector("button").innerText =
-      this.getAttribute("value");
-  }
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector("button").innerText =
+            this.getAttribute("value");
+    }
+
+    //button onclick function
+    testClick() {
+        console.log("test123");
+    }
+
+    //adding event listener to the button
+    connectedCallback() {
+        this.shadowRoot
+            .querySelector("#btnClick")
+            .addEventListener("click", () => this.testClick());
+    }
+
+    //removing event listener on the button
+    disconnectedCallback() {
+        this.shadowRoot.querySelector("#btnClick").removeEventListener();
+    }
 }
 customElements.define("xello-button", XelloButton);
